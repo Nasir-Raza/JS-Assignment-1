5b185439b2ebf0a1a83ea7ef12dd54f344251d8d
Nasir-Raza <send2raza@gmail.com>
2022-05-19 12:02:00 +0500

    Second commit to JS-Assignment-1

--- illegalLegalVariableName.js
+++ illegalLegalVariableName.js
@@ -23,37 +23,3 @@ document.write("<h3>c. Variable names are case <u>sensitive</u></h3>");
 
 document.write("<h3>d. Variable names should not be JS <u>keywords</u></h3>");
 
-/* 
-a. A heading stating “Rules for naming JS variables”
-b. Variable names can only contain ______, ______, 
-______ and ______. 
-For example $my_1stVariable
-c. Variables must begin with a ______, ______ or 
-_____. For example $name, _name or name
-d. Variable names are case _________
-e. Variable names should not be JS _________
--- END -
- */
-/*
-alert("I am " + age + " years old");
-
-var numberOfVists = 21;
-alert("You have visited this site " + numberOfVists + " times");
-
-var birthYear = 1990;
-document.write("<hr/>");
-document.write("<h1>Below text written through Javascript</h2>");
-document.write("<h2>My birth year is " + birthYear + "</h2>");
-document.write("<h2>Data type of my declared variable is " + typeof(birthYear) + "</h2>" );
-
-
-
-
-var visitorName = "John Doe";
-var productTitle = "T-Shirt(s)";
-var quantity = 5;
-
-document.write("<h3>" + visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store</h3>");
-
-document.write("<hr/>");
-*/

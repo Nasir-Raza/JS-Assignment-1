cd68882fa483ab9f148a811276d12bfd44f9c4b9
Nasir-Raza <send2raza@gmail.com>
2022-05-21 17:20:43 +0500

    Third commit to add math and string exercises to JS-Assignment-1

---
+++ concatenateStrings.js
@@ -0,0 +1,34 @@
+var fullName = prompt("Please enter your name");
+alert("Hi, " + fullName);
+
+
+var tableNum = parseInt(prompt("Enter Table number 'must be greter than 0'"));
+
+if (tableNum > 0){
+document.write("<h2>" + tableNum + " x 1 = " + tableNum * 1 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 2 = " + tableNum * 2 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 3 = " + tableNum * 3 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 4 = " + tableNum * 4 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 5 = " + tableNum * 5 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 6 = " + tableNum * 6 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 7 = " + tableNum * 7 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 8 = " + tableNum * 8 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 9 = " + tableNum * 9 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 10 = " + tableNum * 10 + "</h2>");
+}
+else
+{
+    let tableNum = 5;
+    document.write("<h2>" + tableNum + " x 1 = " + tableNum * 1 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 2 = " + tableNum * 2 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 3 = " + tableNum * 3 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 4 = " + tableNum * 4 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 5 = " + tableNum * 5 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 6 = " + tableNum * 6 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 7 = " + tableNum * 7 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 8 = " + tableNum * 8 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 9 = " + tableNum * 9 + "</h2>");
+    document.write("<h2>\n" + tableNum + " x 10 = " + tableNum * 10 + "</h2>");
+    
+}
+
---
+++ mathExpression.js
@@ -0,0 +1,66 @@
+var num1 = parseInt(prompt("Enter first number"));
+var num2 = parseInt(prompt("Enter second number"));
+var result = num1  + num2;
+alert("Sum of " + num1 + " and " + num2 + " is " + result);
+
+result = num1  - num2;
+alert("Result of subtracting " + num1 + " from " + num2 + " is " + result);
+
+result = num1  * num2;
+alert("Result of multiplying " + num1 + " and " + num2 + " is " + result);
+
+result = num1  / num2;
+alert("Result of dividing " + num1 + " by " + num2 + " is " + result);
+
+result = num1  % num2;
+alert("Remainder of dividing " + num1 + " by " + num2 + " is " + result);
+
+var number;
+
+document.write("<h3>Value after variable declaration is: " + number + "</h3>");
+
+number = 3;
+
+document.write("<h3>Initial value is: " + number + "</h3>");
+
+number++;
+
+document.write("<h3>Value after increment is: " + number + "</h3>");
+
+number = number + 7;
+
+document.write("<h3>Value after addition is: " + number + "</h3>");
+
+number--;
+
+document.write("<h3>Value after decrement is: " + number + "</h3>");
+
+number = number % 3;
+
+document.write("<h3>The remainder is: " + number + "</h3>");
+
+var movieTicketCost = 600;
+
+document.write("<h3>Total cost to buy 5 tickets to a movie is " + movieTicketCost * 5  + " PKR</h3>");
+
+var tableNum = parseInt(prompt("Enter Table number"));
+
+document.write("<h2>" + tableNum + " x 1 = " + tableNum * 1 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 2 = " + tableNum * 2 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 3 = " + tableNum * 3 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 4 = " + tableNum * 4 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 5 = " + tableNum * 5 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 6 = " + tableNum * 6 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 7 = " + tableNum * 7 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 8 = " + tableNum * 8 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 9 = " + tableNum * 9 + "</h2>");
+document.write("<h2>\n" + tableNum + " x 10 = " + tableNum * 10 + "</h2>");
+
+
+
+var tempratureCelcius = 12, tempratureFahrenheit = 68;
+
+document.write("<h1>" + tempratureCelcius + "&#8451; is " + ((tempratureCelcius * 9/5) + 32) + "&#8457;</h1>");
+
+document.write("<h1>" + tempratureFahrenheit + "&#8457; is " + ((tempratureFahrenheit -32) * 5/9) + "&#8451;</h1>");
+
